3a7b175ecfaf56fd67bda41cc6a3d016a57c6069
Русакова Алла <allarus88@gmail.com>
2022-09-28 17:15:05 +0300

    Последняя правка по 5 задаче

--- practice2/task5.py
+++ practice2/task5.py
@@ -1,15 +1,16 @@
-from secrets import randbelow #нельзя random - возьмём другой :)
+from secrets import randbelow #нельзя random - возьмём другой :). Продолжаю поиск альтернативного решения
+
 
 
 def myShuffle(some_list):
-    for i in reversed(range(1, len(some_list))):
+    for i in range(1, len(some_list)):
         j = randbelow(i)
         some_list[i], some_list[j] = some_list[j], some_list[i]
 
     return some_list
 
 
-# Проверка:
-# list1 = [8, -17, -12, 10, 3]
-# #print(myShuffle(list1))
+#Проверка:
+list1 = [8, -17, -12, 10, 3]
+print(myShuffle(list1))
 
