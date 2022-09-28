de8887de4a26fed300cfd725a9a1281c54efcf08
Русакова Алла <allarus88@gmail.com>
2022-09-28 18:32:41 +0300

    2 варианта решения 5 задачи

--- practice2/task5.py
+++ practice2/task5.py
@@ -1,16 +1,30 @@
-from secrets import randbelow #нельзя random - возьмём другой :). Продолжаю поиск альтернативного решения
+# Вариант1
+from secrets import randbelow #нельзя random - возьмём другой :)
 
 
-
-def myShuffle(some_list):
+def myShuffle1(some_list):
+    """
+    список на выходе при каждом запуске функции получаем разный
+    """
     for i in range(1, len(some_list)):
         j = randbelow(i)
         some_list[i], some_list[j] = some_list[j], some_list[i]
 
     return some_list
 
+# Вариант2
+from datetime import datetime
 
-#Проверка:
+
+def myShuffle2(some_list):
+    for i in range(0, len(some_list)):
+        j = datetime.now().microsecond % 10 >> 1  # текущее время в микросекундах
+        some_list[i], some_list[j] = some_list[j], some_list[i]
+        # some_list.reverse()
+    return some_list
+
+
+# Проверка:
 list1 = [8, -17, -12, 10, 3]
-print(myShuffle(list1))
-
+print(f"Вариант1 - {myShuffle1(list1)}")
+print(f"Вариант2 - {myShuffle2(list1)}")
